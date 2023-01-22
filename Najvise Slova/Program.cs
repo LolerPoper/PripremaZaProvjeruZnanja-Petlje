6579bf0016d6e13e78b8f8eea97152d05479e00c
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2023-01-22 12:39:05 +0100

    Add files via upload

---
+++ Najvise Slova/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Najvise_Slova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char znak = ' ';
+            int br, brMax = 0;
+
+            Console.WriteLine("Unesite riječ: ");
+            string rijec = Console.ReadLine();
+
+            for(int i =0; i < rijec.Length; i++)
+            {
+                br = 0;
+                for(int j =0; j < rijec.Length; j++)
+                {
+                    if(rijec[i] == rijec[j])
+                    {
+                        br++;
+                    }
+
+                    if(br > brMax)
+                    {
+                        znak = rijec[i];
+                        brMax = br;
+                    }
+                }
+            }
+
+            Console.WriteLine("Najkorišteniji znak je " + znak + ", " + brMax + " puta.");
+
+            Console.ReadKey();
+        }
+    }
+}
